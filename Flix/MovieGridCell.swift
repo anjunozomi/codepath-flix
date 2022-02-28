e869c3f9f7b944ed9fcdeec969db9295ebcdae02
anjunozomi <anj2007@nyu.edu>
2022-02-27 19:21:57 -0800

    Pushed to repository

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Anju Jain on 2/27/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
